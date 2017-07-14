fc006d4019e9f1a6092bd66518772f75eda0da43
Liam <liam3006x@gmail.com>
2017-07-14 23:48:47 +0100

    Add test code to test getting playlist

--- youtube-playlist-details/js/get-playlist.js
+++ youtube-playlist-details/js/get-playlist.js
@@ -1,6 +1,7 @@
 // TODO: REMOVE THIS VAR
 var DEBUG_CHECK_FOR_URL = false; // DEBUG: should we check for a playlist url
 
+var api_key = getApiKey();
 var playlist_id = getPlaylistId();
 
 //TODO: REMOVE CHECK FOR DEBUG URL
@@ -12,4 +13,13 @@ if (!playlist_id && DEBUG_CHECK_FOR_URL) {
 
 // otherwise, a url was entered:
 var span_playlist_id = document.getElementById("playlist-id"); // get the span that will display the url
-span_playlist_id.innerHTML = playlist_id; // display the url
+span_playlist_id.innerHTML = playlist_id + " <br/>" + api_key; // display the url
+
+retrievePlaylist(api_key, playlist_id, showPlaylistInfo);
+
+function showPlaylistInfo(data) {
+    var playlist = getPlaylistFromData(data);
+    for (key in playlist) {
+        alert("Key: " + key + ". Data: " + playlist[key]);
+    }
+}
