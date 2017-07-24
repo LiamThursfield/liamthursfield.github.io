502b7bae299f50d9a7226a6c7f31a0a82fb29162
Liam <liam3006x@gmail.com>
2017-07-24 21:39:39 +0100

    Get playlist details, and show them as a card

--- youtube-playlist-details/js/get-playlist.js
+++ youtube-playlist-details/js/get-playlist.js
@@ -1,5 +1,8 @@
 // TODO: REMOVE THIS VAR
+var DEBUG = true;
 var DEBUG_CHECK_FOR_URL = false; // DEBUG: should we check for a playlist url
+var DEBUG_SHOW_ID_AND_KEY = false;
+
 
 var api_key = getApiKey();
 var playlist_id = getPlaylistId();
@@ -10,16 +13,36 @@ if (!playlist_id && DEBUG_CHECK_FOR_URL) {
     alert("No URL was entered. Re-directing to the home page.");
     window.location.href = 'index.html';
 }
-
-// otherwise, a url was entered:
-var span_playlist_id = document.getElementById("playlist-id"); // get the span that will display the url
-span_playlist_id.innerHTML = playlist_id + " <br/>" + api_key; // display the url
+if (DEBUG) {
+	playlist_id = "PLE7E8B7F4856C9B19";
+}
+if (DEBUG_SHOW_ID_AND_KEY) {
+	var span_playlist_id = document.getElementById("playlist-id"); // get the span that will display the url
+	span_playlist_id.innerHTML = playlist_id + " <br/>" + api_key; // display the url
+}
 
 retrievePlaylist(api_key, playlist_id, showPlaylistInfo);
 
 function showPlaylistInfo(data) {
     var playlist = getPlaylistFromData(data);
-    for (key in playlist) {
-        alert("Key: " + key + ". Data: " + playlist[key]);
-    }
+//	saveText( JSON.stringify(playlist), "filename.json" );
+//	var d = new Date(playlist['publishedAt']);
+//	console.log(getShortMonth(d.getMonth()));
+//    for (key in playlist) {
+//        console.log("Key: " + key + ". Data: " + playlist[key]);
+//    }
+	document.getElementById("playlist-title").innerHTML = playlist['title'];
+	document.getElementById("playlist-author").innerHTML = playlist['channelTitle'];
+	document.getElementById("playlist-description").innerHTML = playlist['description'];
+	document.getElementById("playlist-thumbnail").setAttribute("src",
+        playlist['thumbnails']["high"]["url"]);
+	
+	
+}
+
+function saveText(text, filename){
+	var a = document.createElement('a');
+	a.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(text));
+	a.setAttribute('download', filename);
+	a.click()
 }
