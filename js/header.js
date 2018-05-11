85b4ed08ce3bce51812f3632a8b5badc21f17a48
Liam Thursfield <liam3006x@gmail.com>
2018-05-11 21:30:24 +0100

    Main Site - Update to Monochrome and Gold Site

---
+++ js/header.js
@@ -0,0 +1,9 @@
+/* Open the sidenav */
+function openMobileNavigation() {
+    document.getElementById("mobile-navigation-container").classList.add("mobile-navigation-container-open");
+}
+
+/* Close/hide the sidenav */
+function closeMobileNavigation() {
+    document.getElementById("mobile-navigation-container").classList.remove("mobile-navigation-container-open");
+}
