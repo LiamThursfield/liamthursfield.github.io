3c0435b2356e635e58042c023230ec2759b99d82
Liam Thursfield <liam3006x@gmail.com>
2018-05-17 14:04:25 +0100

    Stop focusing on search bar on key press

--- youtube-playlist-details/js/index.js
+++ youtube-playlist-details/js/index.js
@@ -3,7 +3,5 @@
 * When the window loads, add a keydown listener to focus on the search box
 */
 window.onload = function() {
-	document.addEventListener("keydown", function() {
-		document.getElementsByClassName("search-box")[0].focus();
-	})
+	document.getElementsByClassName("search-box")[0].focus();
 }
